d59932d8c4aa35610cf28c88ae4de48ae29b4fea
larpios <33772093+larpios@users.noreply.github.com>
2026-03-11 21:08:12 +0900

    feat: add helix

---
+++ modules/tools/helix.nix
@@ -0,0 +1,33 @@
+{...}: {
+  flake.modules.homeManager.base = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    programs.helix = {
+      enable = true;
+      package = pkgs.evil-helix;
+      settings = {
+        editor = {
+          evil = true; # evil-helix option
+          line-number = "relative";
+          lsp.display-messages = true;
+          auto-pairs = false;
+          indent-guides.render = true;
+        };
+        keys.normal = {
+          # space.f.f = "file_picker";
+          # space.c.a = "code_action";
+          # space.f.b = "buffer_picker";
+          # space.f.s = "symbol_picker";
+          # space.f.c = "changed_file_picker";
+          # space.f.d = "diagnostics_picker";
+          # space.f."." = "last_picker";
+          # space.w.w = ":w";
+          # space.w.q = ":wq";
+          # space.q.q = ":q";
+        };
+      };
+    };
+  };
+}
