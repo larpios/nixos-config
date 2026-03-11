28b219b2b614e353de5f36ff9dba795621a0d662
larpios <33772093+larpios@users.noreply.github.com>
2026-03-11 23:12:15 +0900

    feat(jujutsu): update config

--- modules/tools/jujutsu.nix
+++ modules/tools/jujutsu.nix
@@ -1,32 +1,70 @@
 # Jujutsu VCS configuration.
 # Contributes to flake.modules.homeManager.base.
-{...}: {
-  flake.modules.homeManager.base = {config, ...}: {
-    programs.jujutsu = {
-      enable = true;
-      settings = {
-        user = {
-          name = config.home.username;
-          email = "kjwdev01@gmail.com";
-        };
-        ui = {
-          diff-formatter = ":git";
-          pager = "delta";
-          editor = "nvim";
-          default-command = "log";
-        };
-        git.default-branch = "main";
-        diff.tool.difftastic.command = ["difft" "--color=always"];
-        revset-aliases."HEAD" = "@-";
-        aliases = {
-          lg = ["log"];
-          lga = ["log-all"];
-          log-all = ["log" "-r" "all()"];
-          df = ["diff"];
-          b = ["bookmark"];
-          n = ["new"];
+{ ... }:
+{
+  flake.modules.homeManager.base =
+    { config, ... }:
+    {
+      programs.jujutsu = {
+        enable = true;
+        settings = {
+          user = {
+            name = config.home.username;
+            email = "kjwdev01@gmail.com";
+          };
+          ui = {
+            diff-formatter = ":git";
+            pager = "delta";
+            color = "always";
+            editor = "nvim";
+            default-command = "log";
+          };
+          template-aliases = {
+            "format_short_change_id(id)" = "change_id.short(8)";
+            "format_short_commit_id(id)" = "commit_id.short(8)";
+          };
+          core = {
+            fsmonitor = "watchman";
+          };
+          git = {
+            default-branch = "main";
+            # Automatically abandon commits that are no longer reachable in Git
+            abandon-locally-abandoned = true;
+            # Ensure we don't try to push to read-only remote branches
+            push-bookmark-prefix = "jj/";
+          };
+          diff.tool.difftastic.command = [
+            "difft"
+            "--color=always"
+          ];
+          revset-aliases."HEAD" = "@-";
+          aliases = {
+            lg = [ "log" ];
+            lga = [ "log-all" ];
+            log-all = [
+              "log"
+              "-r"
+              "all()"
+            ];
+            df = [ "diff" ];
+            dfp = [
+              "diff"
+              "-r"
+              "@-"
+            ];
+            sl = [ "show-last" ];
+            show-last = [
+              "show"
+              "-r"
+              "@-"
+            ];
+            b = [ "bookmark" ];
+            n = [ "new" ];
+          };
+          snapshot = {
+            auto-update-stale = true;
+          };
         };
       };
     };
-  };
 }
