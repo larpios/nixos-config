5d713f2d8ddf7b2a52c7ec78011f21513949735c
larpios <33772093+larpios@users.noreply.github.com>
2026-03-16 14:58:14 +0000

    feat: add NixOS WSL home configuration

--- modules/home-configurations.nix
+++ modules/home-configurations.nix
@@ -10,6 +10,14 @@
       system = "aarch64-darwin";
       module = config.flake.modules.homeManager.base;
     };
+    wsl = {
+      system = "x86_64-linux";
+      module = {
+        imports = [config.flake.modules.homeManager.base];
+        home.homeDirectory = "/home/nixos";
+        home.username = "nixos";
+      };
+    };
     termux = {
       system = "aarch64-linux";
       homeDirectory = "/data/data/com.termux.nix/files/home";
