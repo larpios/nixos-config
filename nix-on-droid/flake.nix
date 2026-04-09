d8b12a238a419b0de4038776651d175802fe1ef1
larpios <33772093+larpios@users.noreply.github.com>
2026-04-09 19:43:08 +0900

    fix(nix-on-droid): separate unstable nixpkgs from 24.05

--- nix-on-droid/flake.nix
+++ nix-on-droid/flake.nix
@@ -2,19 +2,20 @@
   description = "Basic example of Nix-on-Droid system config.";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/prerelease-25.11";
+      url = "github:nix-community/nix-on-droid/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nix-on-droid }: {
+  outputs = { nixpkgs, nix-on-droid, ... }@inputs: {
 
     nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
       pkgs = import nixpkgs { system = "aarch64-linux"; };
       modules = [ ./nix-on-droid.nix ];
+      extraSpecialArgs = { inherit inputs; };
     };
 
   };
--- nix-on-droid/nix-on-droid.nix
+++ nix-on-droid/nix-on-droid.nix
@@ -1,66 +1,78 @@
-{ config, lib, pkgs, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:let
+  unstable = import inputs.nixpkgs-unstable {
+      inherit (pkgs) system;
+  };
+in
 {
   # Simply install just the packages
-  environment.packages = with pkgs; [
-    # User-facing stuff that you really really want to have
-    vim # or some other editor, e.g. nano or neovim
+  environment.packages = with pkgs;
+    [
+      # User-facing stuff that you really really want to have
+      vim # or some other editor, e.g. nano or neovim
 
-    # Some common stuff that people expect to have
-    #procps
-    killall
-    diffutils
-    findutils
-    util-linux
-    tzdata
-    hostname
-    man
-    gnugrep
-    gnupg
-    gnused
-    gnutar
-    bzip2
-    gzip
-    xz
-    zip
-    unzip
-    less
-    curl
-    wget
-    bun
-    git
-    lazygit
-    mise
-    which
-    nushell
-    zellij
-    sudo
-    chezmoi
-    fish
-    neovim
-    nb
-    zoxide
-    fzf
-    ripgrep
-    fd
-    btop
-    rustup
-    direnv
-    gcc
-    yazi
-    evil-helix
-    starship
-    bat
-    btop
-    uv
-    openssh
-    nodejs
-    bun
-    pkg-config
-    jujutsu
-    cargo-binstall
-    nerd-fonts.jetbrains-mono
-  ];
+      # Some common stuff that people expect to have
+      #procps
+      killall
+      diffutils
+      findutils
+      util-linux
+      tzdata
+      hostname
+      man
+      gnugrep
+      gnupg
+      gnused
+      gnutar
+      bzip2
+      gzip
+      xz
+      zip
+      unzip
+      less
+      curl
+      wget
+      bun
+      git
+      lazygit
+      mise
+      which
+      nushell
+      zellij
+      sudo
+      chezmoi
+      fish
+      neovim
+      nb
+      zoxide
+      fzf
+      ripgrep
+      fd
+      btop
+      rustup
+      direnv
+      gcc
+      yazi
+      helix
+      starship
+      bat
+      btop
+      uv
+      openssh
+      nodejs
+      bun
+      pkg-config
+      cargo-binstall
+    ]
+    ++ (with unstable; [
+      nerd-fonts.jetbrains-mono
+      jujutsu
+    ]);
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
@@ -83,7 +95,7 @@
 
   # user.shell = "${lib.getExe pkgs.fish}";
   user.shell = "${pkgs.bash}/bin/bash";
-  
+
   time.timeZone = "Asia/Seoul";
 
   # Set your time zone
